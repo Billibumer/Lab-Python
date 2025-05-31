5dc7891b5e517b77e90f7814775e43ec135bcf5e
Billibumer <markhazov1@mail.ru>
2025-05-31 12:59:59 +0700

    Update Lab9 (ex1).py

--- Lab9 (ex1).py
+++ Lab9 (ex1).py
@@ -1,14 +1,11 @@
 with open('matrix.txt','r') as f:
     lines=f.readlines()
-
 matrix=[list(map(int,line.strip().split(','))) for line in lines]
-
 elements=[elem for row in matrix for elem in row]
 
 s=sum(elements)
 max_elem=max(elements)
 min_elem=min(elements)
-
 print('Сумма:',s)
 print('Максимум:',max_elem)
 print('Минимум:',min_elem)
